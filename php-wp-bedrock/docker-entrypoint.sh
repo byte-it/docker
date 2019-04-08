11a696930be786952ef57cca374a9b43acb35c64
Jan Galler <jan@lets-byte.it>
2019-04-08 20:48:29 +0000

    I have no clue

--- php-wp-bedrock/docker-entrypoint.sh
+++ php-wp-bedrock/docker-entrypoint.sh
@@ -6,9 +6,20 @@ if ! $(wp core is-installed --allow-root); then
     wp core install  --url="${WP_HOME}" --title="${WP_TITLE}" --admin_user=admin --admin_email=admin@lets-byte.it --allow-root
 fi
 
-wp theme activate ${WP_THEME}  --allow-root
-wp plugin activate --all  --allow-root
+# Make sure an updated WordPress can run its database-update
+ wp core update-db --allow-root
 
+# Make sure our custom Theme is always active 
+wp theme activate ${WP_THEME} --allow-root
+
+# Enable all Plugins by default, so new ones get enabled automatically
+wp plugin activate --all --allow-root
+
+# Clear Caches
+wp cache flush --allow-root
+wp transient delete --all --allow-root
+# @TODO: Clear Super-Cache
+# @TODO: Clear Twig-Cache
 
 # Start nginx
 service nginx start
