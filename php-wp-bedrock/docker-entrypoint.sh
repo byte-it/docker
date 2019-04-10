6eda8da0798536dd22dff426d30a6ff9f5b3b979
Jan Galler <jan@lets-byte.it>
2019-04-10 15:59:18 +0000

    Remove todo

--- php-wp-bedrock/docker-entrypoint.sh
+++ php-wp-bedrock/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-# @TODO: Think about wrapping most of this stuff in a DB transaction..
-
 # Bash script for checking whether WordPress is installed or not
 if ! $(wp core is-installed --allow-root); then
     wp core install  --url="${WP_HOME}" --title="${WP_TITLE}" --admin_user=admin --admin_email=admin@lets-byte.it --allow-root
