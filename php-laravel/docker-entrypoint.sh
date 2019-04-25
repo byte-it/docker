fe32b869cc7801f8271436392746fa2e7bed9dcb
Jan Galler <jan@lets-byte.it>
2019-04-25 16:25:18 +0200

    Add pre-running-scripts and make nginx start after setup

--- php-laravel/docker-entrypoint.sh
+++ php-laravel/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Store the current working directory to pass it to further scripts
+WORKING_DIR="$(pwd)"
+
 # Fix permissions
 usermod -a -G www-data root
 chgrp -R www-data storage
@@ -8,13 +11,21 @@ chgrp -R www-data storage
 chown -R www-data:www-data ./storage
 chmod -R 0777 ./storage
 
-# Start nginx
-service nginx start
-
 # Do Laravel things
 php artisan down
 php artisan migrate
 php artisan up
 
+# Call custom scripts for pre-running here
+PRE_SCRIPTS_DIR=/var/scripts/pre
+if [ -d "$PRE_SCRIPTS_DIR" ]; then
+  PRE_SCRIPTS="$PRE_SCRIPTS_DIR/*.sh"
+  for script in $PRE_SCRIPTS; do
+	  [[ -f $script && -x $script ]] && bash $script $WORKING_DIR
+	done
+fi
+
+# Start nginx
+service nginx start
 
 exec "$@"
