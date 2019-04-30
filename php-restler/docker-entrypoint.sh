3956c93becc5ec0391dba7459d8d5e6307c34adb
Jan Galler <jan@lets-byte.it>
2019-04-30 19:50:36 +0200

    Do stuff

--- php-restler/docker-entrypoint.sh
+++ php-restler/docker-entrypoint.sh
@@ -6,10 +6,10 @@ WORKING_DIR="$(pwd)"
 
 # Call custom scripts for pre-running here
 PRE_SCRIPTS_DIR=/var/scripts/pre
-if [ -d "$PRE_SCRIPTS_DIR" ]; then
+if [[ -d "$PRE_SCRIPTS_DIR" ]]; then
   PRE_SCRIPTS="$PRE_SCRIPTS_DIR/*.sh"
-  for script in $PRE_SCRIPTS; do
-	  [[ -f $script && -x $script ]] && bash $script $WORKING_DIR
+  for script in ${PRE_SCRIPTS}; do
+	  [[ -f ${script} && -x ${script} ]] && bash ${script} ${WORKING_DIR}
 	done
 fi
 
